17b48c8484ae49908cf2ef4f1fe6360ac495f90f
KeenanHoffman <keenan.m.hoffman@gmail.com>
2016-04-03 10:16:35 -0600

    added heatmap to front and parser for lat long to back

--- server/modules/data/data.controller.js
+++ server/modules/data/data.controller.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function getOne(req, res, next) {
-  req.models.bus_geocoded.findOne(2).exec(function(err, users) {
+  req.models.bus_geocoded.find({
+    principalZipCode: '80521',
+    entityStatus: 'Good Standing',
+    select: ['entityId', 'entityName', 'Location']
+  }).exec(function(err, business_locations) {
     if (err) {
       next(err);
     } else {
-      res.json(users);
+      business_locations.forEach(function(business_location) {
+        var latLng = business_location.Location.split('(')[1];
+        if(latLng) {
+          latLng = latLng.split(',');
+          latLng[0] = Number(latLng[0]);
+          latLng[1] = Number(latLng[1].split(')')[0]);
+          business_location.latLng = latLng;
+        }
+      });
+      res.json(business_locations);
     }
   });
 }
